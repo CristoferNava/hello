e02b5cbf43f038dc1ce40dabf11b2783bce6b764
CristoferNava <cj.carmonanava@ugto.mx>
2020-11-05 19:43:37 -0600

    hello package was added

---
+++ greet/greet.go
@@ -0,0 +1,13 @@
+package greet
+
+var emoji string = ":D"
+
+// English returns a message in english
+func English() string {
+	return "Hi " + emoji
+}
+
+// Italian re
+func Italian() string {
+	return "Ciao " + emoji
+}
